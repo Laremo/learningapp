2e8040bbe7463902143693f98b94142e6fde86cf
laremo <memoo.lara12306@hotmail.com>
2025-07-11 13:44:52 -0600

    fix: now the title says went

--- obj/Debug/net8.0/learningapp.AssemblyInfo.cs
+++ obj/Debug/net8.0/learningapp.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("learningapp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5eb7aee6eb5e6cca1df8448fa85873da3e456ae1")]
 [assembly: System.Reflection.AssemblyProductAttribute("learningapp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("learningapp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
